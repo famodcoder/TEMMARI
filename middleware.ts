577b73d8f4d19b906fd6364691d8d187568659c0
famodcoder <ifamobio@gmail.com>
2026-05-27 23:45:51 +0100

    feat: add basic auth middleware to protect studio route

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const ADMIN_PASSWORD = process.env.STUDIO_PASSWORD ?? 'temmari-admin'
-
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/studio')) {
     const basicAuth = request.headers.get('authorization')
@@ -11,7 +9,7 @@ export function middleware(request: NextRequest) {
       const authValue = basicAuth.split(' ')[1]
       const [user, password] = atob(authValue).split(':')
 
-      if (user === 'admin' && password === ADMIN_PASSWORD) {
+      if (user === 'admin' && password === (process.env.STUDIO_PASSWORD ?? 'temmari2024')) {
         return NextResponse.next()
       }
     }
